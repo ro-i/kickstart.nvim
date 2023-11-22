ada7a7a852e7d457007e728452e91147cbd0cba9
Robert Imschweiler <ro-i@posteo.de>
2023-11-22 10:39:02 +0100

    Improve neotree keybindings.

--- init.lua
+++ init.lua
@@ -242,8 +242,6 @@ vim.o.scrolloff = 5
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
-vim.keymap.set('n', 'E', '<Cmd>Neotree toggle current reveal_force_cwd<CR>')
-
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
