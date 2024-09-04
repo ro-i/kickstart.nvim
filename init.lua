7d9d589aeed20cc33dbafcd3292957a284a907e3
Robert Imschweiler <ro-i@posteo.de>
2024-09-04 10:18:02 +0200

    fix python autoformatting

--- init.lua
+++ init.lua
@@ -774,7 +774,7 @@ require('lazy').setup({
       notify_on_error = false,
       formatters_by_ft = {
         -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
+        python = { "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
